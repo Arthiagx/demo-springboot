a5eb60e4ecdb6d7af9ef00a634ebbf39a5d9073b
Thiago Henrique <153330691+Arthiagx@users.noreply.github.com>
2024-03-05 17:56:24 -0300

    Exception handlings

--- src/main/java/com/arthiagx/demo/services/UserService.java
+++ src/main/java/com/arthiagx/demo/services/UserService.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.arthiagx.demo.entities.User;
 import com.arthiagx.demo.repositories.UserRepository;
+import com.arthiagx.demo.services.exceptions.DatabaseException;
+import com.arthiagx.demo.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,6 +24,28 @@ public class UserService {
 	}	
 	public User findbyId(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());		
 	}
 }
