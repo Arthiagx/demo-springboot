5bcc6d91d73cb93f430be90059bee5c8e46f4e70
Thiago Henrique <153330691+Arthiagx@users.noreply.github.com>
2024-03-05 16:14:27 -0300

    Product-OrderItem one-to-many association Payment, one-to-one association Subtotal & Total methods

--- src/main/java/com/arthiagx/demo/config/TestConfig.java
+++ src/main/java/com/arthiagx/demo/config/TestConfig.java
@@ -10,10 +10,13 @@ import org.springframework.context.annotation.Profile;
 
 import com.arthiagx.demo.entities.Category;
 import com.arthiagx.demo.entities.Order;
+import com.arthiagx.demo.entities.OrderItem;
+import com.arthiagx.demo.entities.Payment;
 import com.arthiagx.demo.entities.Product;
 import com.arthiagx.demo.entities.User;
 import com.arthiagx.demo.entities.enums.OrderStatus;
 import com.arthiagx.demo.repositories.CategoryRepository;
+import com.arthiagx.demo.repositories.OrderItemRepository;
 import com.arthiagx.demo.repositories.OrderRepository;
 import com.arthiagx.demo.repositories.ProductRepository;
 import com.arthiagx.demo.repositories.UserRepository;
@@ -33,6 +36,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -70,5 +76,17 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
--- src/main/java/com/arthiagx/demo/entities/Product.java
+++ src/main/java/com/arthiagx/demo/entities/Product.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,11 +13,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable {	
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -25,15 +28,16 @@ public class Product implements Serializable {
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
+
 	@ManyToMany
-	@JoinTable(name = "tb_product_category",
-	joinColumns = @JoinColumn(name = "product_id"),
-	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product() {
-		}
+	}
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
@@ -41,7 +45,7 @@ public class Product implements Serializable {
 		this.name = name;
 		this.description = description;
 		this.price = price;
-		this.imgUrl = imgUrl;	
+		this.imgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -86,6 +90,14 @@ public class Product implements Serializable {
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
